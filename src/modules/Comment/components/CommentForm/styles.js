97ee96e119e270d1ffb66cf5a786c96f6567b911
Katsiaryna Lunchuk <katsiarynalunchuk@mac-205s-Mac-mini.local>
2018-05-10 11:48:01 +0300

    Add form comment. Instal a few npm package

---
+++ src/modules/Comment/components/CommentForm/styles.js
@@ -0,0 +1,19 @@
+const styles = () => ({
+    form: {
+        display: 'grid',
+        gridTemplateColumns: '138px 1fr',
+        gridColumnGap: '10px',
+        marginTop: '35px'
+    },
+    button: {
+        display: 'inline-block',
+        background: 'linear-gradient(top, #4caf50 0%, #a5c956 100%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 40,
+        padding: '0 10px'
+    }
+});
+
+export default styles;
